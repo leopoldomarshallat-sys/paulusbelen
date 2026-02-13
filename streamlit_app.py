65d9ad3048fbbc697730af5ccffd1dda7b550b9a
leopoldomarshallat-sys <leopoldomarshallat@gmail.com>
2026-02-13 08:43:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Menghitung  :blue[Volume Tabung] :rocket:")
 
 r = st.number_input("Masukan Jari-Jari (cm): ",0)
 r = st.number_input("Masukan Tinggi (cm): ",0)
+if  st.button("Hitung Volume", type="primary):
